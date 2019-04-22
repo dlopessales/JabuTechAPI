eebb5c5e8ba1efcfbba8e49d1ab6bbfcbbdd0327
Danilo <35387644+dlopessales@users.noreply.github.com>
2019-04-22 16:18:18 -0300

    Mongo conect deprecated

--- src/database.js
+++ src/database.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
-const server = '172.17.0.2:27017'; // REPLACE WITH YOUR DB SERVER
-const database = 'jabutech';      // REPLACE WITH YOUR DB NAME
+const server = 'localhost:27017'; // REPLACE WITH YOUR DB SERVER
+const database = 'jabutechdb';    // REPLACE WITH YOUR DB NAME
 class Database {
   constructor() {
     this._connect()
   }
 _connect() {
      console.log('on connnect')
-     mongoose.connect(`mongodb://${server}/${database}`)
+     mongoose.connect(`mongodb://${server}/${database}`, { useNewUrlParser: true })
        .then(() => {
-         console.log('Database connection successful')
+         console.log('Database connection successful :)')
        })
        .catch(err => {
-         console.error('Database connection error')
+         console.error('Database connection error :(')
        })
   }
 }
